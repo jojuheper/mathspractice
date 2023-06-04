8674c7859dddb5284d6f77bb10ed509cea4757de
jojuheper <jojuheper@gmail.com>
2023-06-04 18:23:54 +0100

    Add: symbol constant

---
+++ src/main/java/com/example/demo/util/helper/Constant.java
@@ -0,0 +1,11 @@
+package com.example.demo.util.helper;
+
+public final class Constant {
+
+    public final class Symbol{
+        public final static String PLUS = "+";
+        public final static String MINUS = "-";
+        public final static String MULTIPLY = "*";
+        public final static String DIVIDE = "/";
+    }
+}
